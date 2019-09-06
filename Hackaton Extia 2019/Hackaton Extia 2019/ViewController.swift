75b07386b5d064f5d168699c4dd9db078c942d2a
vacca_j <jordan.vacca@epitech.eu>
2019-09-06 20:49:01 +0200

    Init

---
+++ Hackaton Extia 2019/Hackaton Extia 2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackaton Extia 2019
+//
+//  Created by Jordan on 06/09/2019.
+//  Copyright © 2019 Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
